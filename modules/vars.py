984c6fa422835fc17f0bb2373e303659e8f91745
Fan744 <135441979+Fan744@users.noreply.github.com>
2025-11-01 00:24:52 +0530

    Update vars.py with hardcoded API credentials
    Replaced environment variable defaults with hardcoded values for API_ID, API_HASH, and BOT_TOKEN.

--- modules/vars.py
+++ modules/vars.py
@@ -1,8 +1,8 @@
 import os
 
-API_ID    = os.environ.get("API_ID", "")
-API_HASH  = os.environ.get("API_HASH", "")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
+API_ID    = os.environ.get("API_ID", "8313201920")
+API_HASH  = os.environ.get("API_HASH", "3362cbcff56b786d82202086baab0bb7")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8313201920:AAH1PfXk6b6sgBPNCT_H5AEMAhZETItO5gg") 
 
 WEBHOOK = True  # Don't change this
 PORT = int(os.environ.get("PORT", 8080))  # Default to 8000 if not set
