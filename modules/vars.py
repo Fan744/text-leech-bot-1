a1fab07a4dce02e1f07ab2cd32f509e0d3ae8091
Fan744 <135441979+Fan744@users.noreply.github.com>
2025-11-01 00:26:15 +0530

    Change API_ID, API_HASH, and BOT_TOKEN values
    Updated API credentials in vars.py.

--- modules/vars.py
+++ modules/vars.py
@@ -1,8 +1,8 @@
 import os
 
-API_ID    = os.environ.get("API_ID", "28962746")
-API_HASH  = os.environ.get("API_HASH", "727457f88d661b08e636188a949cd9f3")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "7717913508:AAF5BDfhHV4eLSFLqYWGdKMtwF6v5eAEs_g") 
+API_ID    = os.environ.get("API_ID", "8313201920")
+API_HASH  = os.environ.get("API_HASH", "3362cbcff56b786d82202086baab0bb7")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8313201920:AAH1PfXk6b6sgBPNCT_H5AEMAhZETItO5gg") 
 
 WEBHOOK = True  # Don't change this
 PORT = int(os.environ.get("PORT", 8080))  # Default to 8000 if not set
